01a4d4aa561eab00a81f014d339a57d7080edf3a
amirodan <amirodan@gmail.com>
2020-04-29 11:07:45 +0300

    housekeeping2 - proxy settings

--- public/app/background.js
+++ public/app/background.js
@@ -2,7 +2,7 @@
 
     // on hyperlink click start listener
     chrome.history.onVisited.addListener(function(HistoryItem) {
-        // get the tab that was opened and close it
+        // get the tab that was opened (current tab) and close it
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             var currentURL = tabs[0].url;
             console.log(currentURL);
@@ -20,6 +20,22 @@
             console.log("this is a proksee link!");
             var newURL = "http://stackoverflow.com/";
             chrome.tabs.create({ url: newURL });
+            // change the proxy to the one in the link parameter
+            var config = {
+                mode: "fixed_servers",
+                rules: {
+                    proxyForHttp: {
+                        scheme: "http",
+                        host: "127.0.0.1",
+                        port: 8080
+                    },
+                    bypassList: ["foobar.com"]
+                }
+            };
+            chrome.proxy.settings.set(
+                {value: config, scope: 'regular'},
+                function() {});
+            // save the tab id and the proxy data in a dictionary - when we switch tabs we want to change the proxy settings
             return;
         };
 
@@ -30,5 +46,3 @@
 
 }());
 
-//HistoryItem.url == "http://proksee.co/"
-// check that we are in proksee site
