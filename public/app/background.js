2e02287afaea6e1a7d47dd6ea74e3f0ce7e7bf3f
amirodan <amirodan@gmail.com>
2020-04-29 19:27:00 +0300

    listener for tab change

--- public/app/background.js
+++ public/app/background.js
@@ -1,6 +1,13 @@
-(function() {
 
-    // on hyperlink click start listener
+(function() {
+    var proxyconfig = {};
+
+    // create listener for tab change
+    chrome.tabs.onActivated.addListener(({tabId, windowId}) =>  {
+    console.log(tabId);
+    //console.log(url);
+    });
+    // create listener for hyperlink click
     chrome.history.onVisited.addListener(function(HistoryItem) {
         // get the tab that was opened (current tab) and close it
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
@@ -10,32 +17,40 @@
                 chrome.tabs.remove(tabs[0].id);
             }
             return;
-            //if (currentURL = "https://proksee.co/") {
-            //    console.log("we are in proksee site");
-            //    return;
-
         });
+
         // on url click gather proxy info and open incognito tab
         if (HistoryItem.url.includes("?prokseelink")) {
             console.log("this is a proksee link!");
             var newURL = "http://stackoverflow.com/";
             chrome.tabs.create({ url: newURL });
-            // change the proxy to the one in the link parameter
-            var config = {
-                mode: "fixed_servers",
-                rules: {
-                    proxyForHttp: {
-                        scheme: "http",
-                        host: "127.0.0.1",
-                        port: 8080
-                    },
-                    bypassList: ["foobar.com"]
-                }
-            };
-            chrome.proxy.settings.set(
-                {value: config, scope: 'regular'},
-                function() {});
-            // save the tab id and the proxy data in a dictionary - when we switch tabs we want to change the proxy settings
+
+            // get our new prokseetab id
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                var prokseetab = tabs[0].id;
+
+            // change the proxy to the one in the proksee link parameter and
+                // save it with the current tabid
+            proxyconfig[prokseetab] =
+                {
+                    mode: "fixed_servers",
+                    rules: {
+                        proxyForHttp: {
+                            scheme: "http",
+                            host: "127.0.0.1",
+                            port: 8080
+                        },
+                        bypassList: ["foobar.com"]
+                    }
+
+                };
+            console.log("our saved proxy info - tabid:", prokseetab)
+            console.log(proxyconfig[prokseetab]);
+            });
+            //chrome.proxy.settings.set(
+            //    {value: proxyconfig, scope: 'regular'},
+            //    function() {});
+
             return;
         };
 
